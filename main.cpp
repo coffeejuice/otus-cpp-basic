eeca198070ec18a25c514a418db99c87da14ff70
Alex on Laptop <alex.troshin@outlook.com>
2025-02-26 17:36:38 +0800

    save point

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "mainConfig.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << argv[0] << "Version " << sea_battle_VERSION_MAJOR << "."
+        << sea_battle_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0]  << " number" << std::endl;
+        return EXIT_SUCCESS;
+    }
+}
