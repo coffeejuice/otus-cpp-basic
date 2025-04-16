61ba92f5f78af14d8dfa80be02c4a52102a91c25
Alex on Laptop <alex.troshin@outlook.com>
2025-04-16 17:33:10 +0800

    add class Max

--- statistics.cpp
+++ statistics.cpp
@@ -3,65 +3,91 @@
 
 class IStatistics {
 public:
-	virtual ~IStatistics() {}
+    virtual ~IStatistics() = default;
 
-	virtual void update(double next) = 0;
-	virtual double eval() const = 0;
-	virtual const char * name() const = 0;
+    virtual void update(double next) = 0;
+
+    virtual double eval() const = 0;
+
+    virtual const char *name() const = 0;
 };
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
-	}
+    Min() : m_min{std::numeric_limits<double>::min()} {
+    }
 
-	void update(double next) override {
-		if (next < m_min) {
-			m_min = next;
-		}
-	}
+    void update(const double next) override {
+        // std::cout << ", " << next << std::endl;
+        if (next < m_min) {
+            m_min = next;
+        }
+    }
 
-	double eval() const override {
-		return m_min;
-	}
+    double eval() const override {
+        return m_min;
+    }
 
-	const char * name() const override {
-		return "min";
-	}
+    const char *name() const override {
+        return "min";
+    }
 
 private:
-	double m_min;
+    double m_min;
+};
+
+class Max : public IStatistics {
+public:
+    Max() : m_max{std::numeric_limits<double>::max()} {
+    }
+
+    void update(const double next) override {
+        if (next > m_max) {
+            m_max = next;
+        }
+    }
+
+    double eval() const override {
+        return m_max;
+    }
+
+    const char *name() const override {
+        return "max";
+    }
+
+private:
+    double m_max;
 };
 
 int main() {
+    const size_t statistics_count = 2;
+    IStatistics *statistics[statistics_count];
 
-	const size_t statistics_count = 1;
-	IStatistics *statistics[statistics_count];
+    statistics[0] = new Min{};
+    statistics[1] = new Max{};
 
-	statistics[0] = new Min{};
+    double val = 0;
+    while (std::cin >> val) {
+        for (size_t i = 0; i < statistics_count; ++i) {
+            statistics[i]->update(val);
+        }
+    }
 
-	double val = 0;
-	while (std::cin >> val) {
-		for (size_t i = 0; i < statistics_count; ++i) {
-			statistics[i]->update(val);
-		}
-	}
+    // Handle invalid input data
+    // if (!std::cin.eof() && !std::cin.good()) {
+    //     std::cerr << "Invalid input data\n";
+    //     return 1;
+    // }
 
-	// Handle invalid input data
-	if (!std::cin.eof() && !std::cin.good()) {
-		std::cerr << "Invalid input data\n";
-		return 1;
-	}
+    // Print results if any
+    for (size_t i = 0; i < statistics_count; ++i) {
+        std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+    }
 
-	// Print results if any
-	for (size_t i = 0; i < statistics_count; ++i) {
-		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
-	}
+    // Clear memory - delete all objects created by new
+    for (size_t i = 0; i < statistics_count; ++i) {
+        delete statistics[i];
+    }
 
-	// Clear memory - delete all objects created by new
-	for (size_t i = 0; i < statistics_count; ++i) {
-		delete statistics[i];
-	}
-
-	return 0;
+    return 0;
 }
