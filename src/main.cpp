4f0971704d5de82cceb20b8e8f62d5ae45e9fcca
Alex on Laptop <alex.troshin@outlook.com>
2025-04-29 18:33:20 +0800

    small test in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-
 #include <vector>
 #include <algorithm>
 
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& values)
+{
+    for (auto& v : values)
+        os << v << ' ';
+    return os;
+}
+
 int main()
 {
-    std::vector<int> target[10, 0];
-    for (int i = 0; i < 10; i++)
-        std::cout << i << std::endl;
+    std::vector<int> target(10, 0);
+    std::cout << target << std::endl;
+    std::generate(target.begin(), target.end(), [counter = 0]() mutable {return counter += 2;});
+    std::cout << target << std::endl;
     return 0;
 }
