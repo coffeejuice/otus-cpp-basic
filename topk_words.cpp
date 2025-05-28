90baac5b4c45f375b8c734016c538efc72cb9319
Home Alex <alex.troshin@outlook.com>
2025-05-28 08:10:12 +0800

    OTUS source files

--- topk_words.cpp
+++ topk_words.cpp
@@ -15,13 +15,13 @@ const size_t TOPK = 10;
 
 using Counter = std::map<std::string, std::size_t>;
 
-std::string tolower(const std::string &str);
+std::string tolower(const std::string& str);
 
 void count_words(std::istream& stream, Counter&);
 
 void print_topk(std::ostream& stream, const Counter&, const size_t k);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "Usage: topk_words [FILES...]\n";
         return EXIT_FAILURE;
@@ -37,14 +37,15 @@ int main(int argc, char *argv[]) {
         }
         count_words(input, freq_dict);
     }
+    auto end = std::chrono::high_resolution_clock::now();
 
     print_topk(std::cout, freq_dict, TOPK);
-    auto end = std::chrono::high_resolution_clock::now();
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     std::cout << "Elapsed time is " << elapsed_ms.count() << " us\n";
+    return EXIT_SUCCESS;
 }
 
-std::string tolower(const std::string &str) {
+std::string tolower(const std::string& str) {
     std::string lower_str;
     std::transform(std::cbegin(str), std::cend(str),
                    std::back_inserter(lower_str),
@@ -55,7 +56,7 @@ std::string tolower(const std::string &str) {
 void count_words(std::istream& stream, Counter& counter) {
     std::for_each(std::istream_iterator<std::string>(stream),
                   std::istream_iterator<std::string>(),
-                  [&counter](const std::string &s) { ++counter[tolower(s)]; });    
+                  [&counter](const std::string& s) { ++counter[tolower(s)]; });
 }
 
 void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
@@ -67,12 +68,12 @@ void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
 
     std::partial_sort(
         std::begin(words), std::begin(words) + k, std::end(words),
-        [](auto lhs, auto &rhs) { return lhs->second > rhs->second; });
+        [](auto lhs, auto& rhs) { return lhs->second > rhs->second; });
 
     std::for_each(
         std::begin(words), std::begin(words) + k,
-        [&stream](const Counter::const_iterator &pair) {
+        [&stream](const Counter::const_iterator& pair) {
             stream << std::setw(4) << pair->second << " " << pair->first
-                      << '\n';
+                << '\n';
         });
 }
